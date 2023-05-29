32dc6aab47e305797fa4917e55aa0588f316a275
Paul Narvik <111660098+PaulNarvik@users.noreply.github.com>
2023-05-29 20:50:02 +0200

    Modification de la taille de l'écran

--- scripts/game.py
+++ scripts/game.py
@@ -7,15 +7,22 @@ class Game:
     """Classe contenant les variables et méthodes relatives à la fenêtre et au jeu en général"""
 
     def __init__(self) -> None:
-        # Caractéristiques du moniteur
-        self.SCREEN_MAX_WIDTH = pygame.display.get_desktop_sizes()[0][1]
-        self.SCREEN_WIDTH = round(self.SCREEN_MAX_WIDTH)
-        self.SCREEN_HEIGHT = round(self.SCREEN_WIDTH * 9 / 16)
-        
+        # Caractéristiques du moniteur et choix des dimensions
+        self.SCREEN_MAX_SIZE = pygame.display.get_desktop_sizes()[0]
+        self.SCREEN_WIDTH = round(self.SCREEN_MAX_SIZE[0] * 0.80)
+        self.SCREEN_HEIGHT = round(self.SCREEN_WIDTH * 90 / 16)
+
+        # Gestion du cas où la fenêtre dépasse de l'écran
+        if self.SCREEN_HEIGHT > round(self.SCREEN_MAX_SIZE[1] * 0.95):
+            self.SCREEN_HEIGHT = round(self.SCREEN_MAX_SIZE[1] * 0.9)
+            self.SCREEN_WIDTH = round(self.SCREEN_HEIGHT * 16 / 9)
+
         # Création et réglage de la fenêtre
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.RESIZABLE)
         pygame.display.set_caption("Car Game")
         #pygame.display.set_icon()
+
+        self.scenes = {}
 
         self.clock = pygame.time.Clock()
         self.FPS = 30
@@ -40,3 +47,6 @@ class Game:
         self.running = False
         pygame.quit()
         sys.exit()
+
+    def get_marges(self) -> tuple[int, int]:
+        """Fonction renvoyant la marge en fonction de la taille de l'écran (permet d'obtenir un ratio 16/9)"""
